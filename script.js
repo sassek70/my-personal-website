a5a63e63f9fccdcc540289619ea3d8440449d19d
Kevin Sasse <sassek70@gmail.com>
2022-09-03 16:44:13 -0400

    added scrolling block to block runner game

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-// const blockRunner = get
+const block = document.getElementById('block');
+// const hole = document.getElementById('hole');
 
+block.addEventListener('animationiteration', () => {
+    const random = parseInt(-(Math.random()*300)-50);
+    block.style.bottom = random + "px";
+    block.textContent = random + "px";
+    block.style.fontSize = "20px";
+});
