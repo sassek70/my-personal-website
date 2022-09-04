4bc314576321527781d6e9b174ad476e2f805d11
Kevin Sasse <sassek70@gmail.com>
2022-09-03 23:13:22 -0400

    added player block, jump function and gravity

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 const block = document.getElementById('block');
+const player = document.getElementById('player');
+
 // const hole = document.getElementById('hole');
 
 block.addEventListener('animationiteration', () => {
-    const random = parseInt(-(Math.random()*300)-50);
-    block.style.bottom = random + "px";
-    block.textContent = random + "px";
+    const randomBottom = parseInt(-(Math.random()*300)-50);
+    const randomWide = parseInt(Math.random()*100)+40;
+    block.style.bottom = randomBottom + "px";
+    block.style.width = randomWide + "px";
+    block.textContent = randomBottom + "px";
     block.style.fontSize = "20px";
 });
+
+
+
+setInterval(function(){
+        const charTop = parseInt(window.getComputedStyle(player).getPropertyValue("bottom"));
+        player.style.bottom = (charTop - 3) + "px";
+},10);
+
+function playerJump(){
+        const jump = player.style.bottom.replace('px','')
+        const playerJump = parseInt(jump, 10)
+        player.style.bottom = `${playerJump + 50}px`;
+}''
+
+
+
+document.addEventListener("keydown", function (e){
+    if (e.key === "ArrowUp") {
+        playerJump()    
+    }
+})
